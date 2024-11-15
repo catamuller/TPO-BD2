3d7877c64813e53745d7e2a1a8dfe5666bc16636
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-11-15 15:34:57 -0300

    Only instanciate the DB once
    Co-Authored-By: Uriel Arias <96993984+ursath@users.noreply.github.com>

--- .devcontainer/populate.sh
+++ .devcontainer/populate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MARKER_FILE="/workspace/.initialized"
+MARKER_FILE=".initialized"
 
 if [ -f "$MARKER_FILE" ]; then
   echo "Already initialized"
@@ -9,4 +9,5 @@ fi
 
 mongosh "mongodb://mongo:27017" -f ./mongo/schema/*.mongodb.js
 redis-cli -h redis -p 6379 < ./redis/schema/productos.redis
-touch "$MARKER_FILE"
+
+echo "Delete me if you want to rerun the schema files on the devcontainer" > "$MARKER_FILE"
