866da4c8b9a8d9a5f5824c29a82528d06eb33eee
Uriel Arias <96993984+ursath@users.noreply.github.com>
2024-11-15 20:58:08 +0000

    feat: adding query10 with @Secreto31126

---
+++ queries/query10.mongodb.js
@@ -0,0 +1,29 @@
+db.factura.aggregate([
+    {
+        $lookup: {
+            from: "cliente",
+            localField: "nro_cliente",
+            foreignField: "_id",
+            as: "info_cliente"
+        }
+    },
+    {
+        $unwind: "$info_cliente"
+    },
+    {
+        $group: {
+            _id: "$nro_cliente",
+            total_iva: { $sum: "$total_con_iva" },
+            nombre: { $first: "$info_cliente.nombre" },
+            apellido: { $first: "$info_cliente.apellido" }
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            nombre: 1,
+            apellido: 1,
+            total_iva: 1
+        }
+    }
+]);
