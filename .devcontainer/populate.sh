fcb1a5c978e40f2b2b37d1f221485fd8c76f8b50
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-11-15 14:38:30 -0300

    Mini fixes

--- .devcontainer/populate.sh
+++ .devcontainer/populate.sh
@@ -1,2 +1,2 @@
-mongosh -u "mongodb://mongo:27017" -f /workspace/mongo/schema/*.mongodb.js
-redis-cli -h redis -p 6379 < /workspace/redis/schema/productos.schema
+mongosh "mongodb://mongo:27017" -f /workspace/mongo/schema/*.mongodb.js
+redis-cli -h redis -p 6379 < /workspace/redis/schema/productos.redis
