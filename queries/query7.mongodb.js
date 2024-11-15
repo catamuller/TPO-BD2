ed6bb684950c0739b6722de8881777f3802392fa
Uriel Arias <96993984+ursath@users.noreply.github.com>
2024-11-15 20:13:12 +0000

    feat: adding query7

---
+++ queries/query7.mongodb.js
@@ -0,0 +1,24 @@
+db.factura.aggregate([
+   { 
+        $lookup: {
+            from: "cliente",
+            foreignField: "_id",
+            localField: "nro_cliente",
+            as: "info_cliente" 
+        }
+   },
+   {
+    $unwind: "$info_cliente"
+   },
+    { $match: {"info_cliente.nombre": "Kai", "info_cliente.apellido": "Bullock"}},
+    {
+        $project: {
+            _id: 1,
+            fecha: 1,
+            iva: 1,
+            total_con_iva: 1,
+            total_sin_iva: 1,
+            nro_cliente: 1
+        }
+    }
+]);
