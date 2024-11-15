25be283159b54255ad29fd17e39266cddf89647d
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-11-15 15:11:09 -0300

    Testing something
    Co-Authored-By: Uriel Arias <96993984+ursath@users.noreply.github.com>

--- .devcontainer/populate.sh
+++ .devcontainer/populate.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
+
+MARKER_FILE="/workspace/.initialized"
+
+if [ -f "$MARKER_FILE" ]; then
+  echo "Already initialized"
+  exit 0
+fi
+
 mongosh "mongodb://mongo:27017" -f ./mongo/schema/*.mongodb.js
 redis-cli -h redis -p 6379 < ./redis/schema/productos.redis
+touch "$MARKER_FILE"
