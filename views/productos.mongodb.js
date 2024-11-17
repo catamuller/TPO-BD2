fc685d0bad12f0a6558f48089e2da5fe4d0d37c5
agussang <msanguinetti@itba.edu.ar>
2024-11-16 21:10:28 -0300

    Fixed product view

--- views/productos.mongodb.js
+++ views/productos.mongodb.js
@@ -1,14 +1,17 @@
 // Se necesita una vista que devuelva todos los productos que aún no han sido facturados.
+// todo: testear
 
 import getProducts from "../redis/functions";
 
 export default async function ( mongo, redis ) {
 
+    const factura = mongo.collection("factura");
+
     const codigo_productos = await redis.keys("product:*");
-    const codigo_productos_facturados = db.factura.distinct("detalle.codigo_producto");
+    const codigo_productos_facturados = factura.distinct("detalle.codigo_producto");
 
     const result = codigo_productos.filter(x => !codigo_productos_facturados.includes( parseInt(x.split(":")[1], 10) ));
 
-    printjson( await getProducts(result) );
+    return await getProducts(result);
 
 }
