fc8df0fd52f40162d8e8e45c32cefd76481c92d5
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-11-16 22:14:23 -0300

    Updated queries endpoint
    Co-Authored-By: catamuller <108447280+catamuller@users.noreply.github.com>
    Co-Authored-By: Uriel Arias <96993984+ursath@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 import express from "express";
 import { createClient } from "redis";
 import { MongoClient } from "mongodb";
+import query1 from "../queries/query1.js";
+import query2 from "../queries/query2.js";
+import query3 from "../queries/query3.js";
+import query4 from "../queries/query4.js";
+import query5 from "../queries/query5.js";
+import query6 from "../queries/query6.js";
+import query7 from "../queries/query7.js";
+import query8 from "../queries/query8.js";
+import query9 from "../queries/query9.js";
+import query10 from "../queries/query10.js";
 
 const app = express();
 app.use(express.static("public"));
@@ -10,6 +20,19 @@ const redis = createClient({
 });
 
 const mongo = new MongoClient("mongodb://mongo:27017");
+
+const queries = {
+    1: () => query1(mongo.db("facturacion")),
+    2: () => query2(mongo.db("facturacion")),
+    3: () => query3(mongo.db("facturacion")),
+    4: () => query4(mongo.db("facturacion")),
+    5: () => query5(mongo.db("facturacion")),
+    6: () => query6(mongo.db("facturacion")),
+    7: () => query7(mongo.db("facturacion")),
+    8: () => query8(mongo, redis),
+    9: () => query9(mongo, redis),
+    10: () => query10(mongo.db("facturacion"))
+};
 
 app.get("/", (req, res) => {
     res.sendFile("/public/index.html", { root: import.meta.dirname });
@@ -24,10 +47,10 @@ app.get("/debugger", async (req, res) => {
 
 app.get("/:n", async (req, res) => {
     try {
-        const n = parseInt(req.params.n);
-        // const data = await mongoExecute(n);
-        res.send("data").status(200);
+        const data = (await queries[req.params.n]?.()) ?? "Query not found";
+        res.send(data).status(200);
     } catch (error) {
+        console.error(error);
         res.status(500).send(error);
     }
 });
