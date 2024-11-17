f77dd01b37941559fbf3dbaee01f11afbd4c9f6c
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-11-17 01:07:49 -0300

    Testing new data structure

---
+++ mongo/schema/productos.mongodb.js
@@ -0,0 +1,605 @@
+// @ts-nocheck
+/* global use, db */
+
+use("facturacion");
+
+db.producto.insertOne({
+    _id: 1,
+    marca: "Dolor Sit Incorporated",
+    nombre: "return policy",
+    descripcion: "risus. In mi"
+});
+db.producto.insertOne({
+    _id: 2,
+    marca: "A Felis Ullamcorper Company",
+    nombre: "sales tax",
+    descripcion: "Nunc mauris elit,"
+});
+db.producto.insertOne({
+    _id: 3,
+    marca: "Elit A Corp.",
+    nombre: "household goods",
+    descripcion: "nec, euismod in,"
+});
+db.producto.insertOne({
+    _id: 4,
+    marca: "Maecenas Libero Est PC",
+    nombre: "pet supplies",
+    descripcion: "lorem, luctus ut,"
+});
+db.producto.insertOne({
+    _id: 5,
+    marca: "Odio Corp.",
+    nombre: "scales",
+    descripcion: "molestie arcu. Sed"
+});
+db.producto.insertOne({
+    _id: 6,
+    marca: "Et Risus Quisque LLC",
+    nombre: "coin",
+    descripcion: "vestibulum massa rutrum"
+});
+db.producto.insertOne({
+    _id: 7,
+    marca: "Odio Auctor Incorporated",
+    nombre: "price",
+    descripcion: "Praesent interdum ligula"
+});
+db.producto.insertOne({
+    _id: 8,
+    marca: "Elit Pellentesque Consulting",
+    nombre: "meat",
+    descripcion: "ante, iaculis nec,"
+});
+db.producto.insertOne({
+    _id: 9,
+    marca: "Posuere LLP",
+    nombre: "pet supplies",
+    descripcion: "rutrum magna. Cras"
+});
+db.producto.insertOne({
+    _id: 10,
+    marca: "Lobortis Nisi Nibh Consulting",
+    nombre: "credit card",
+    descripcion: "Ut tincidunt vehicula"
+});
+db.producto.insertOne({
+    _id: 11,
+    marca: "Erat Institute",
+    nombre: "bakery",
+    descripcion: "Suspendisse aliquet molestie"
+});
+db.producto.insertOne({
+    _id: 12,
+    marca: "Aptent Taciti Incorporated",
+    nombre: "fish",
+    descripcion: "urna convallis erat,"
+});
+db.producto.insertOne({
+    _id: 13,
+    marca: "Fusce Diam Nunc Ltd",
+    nombre: "pet supplies",
+    descripcion: "tristique senectus et"
+});
+db.producto.insertOne({
+    _id: 14,
+    marca: "Sit Corporation",
+    nombre: "meat",
+    descripcion: "nec ligula consectetuer"
+});
+db.producto.insertOne({
+    _id: 15,
+    marca: "Ligula Aliquam Incorporated",
+    nombre: "trolley",
+    descripcion: "ac mattis ornare,"
+});
+db.producto.insertOne({
+    _id: 16,
+    marca: "Vel Foundation",
+    nombre: "nutritional information",
+    descripcion: "turpis. Nulla aliquet."
+});
+db.producto.insertOne({
+    _id: 17,
+    marca: "Luctus Vulputate Corp.",
+    nombre: "sales assistant",
+    descripcion: "sit amet massa."
+});
+db.producto.insertOne({
+    _id: 18,
+    marca: "Vitae Foundation",
+    nombre: "receipt",
+    descripcion: "magnis dis parturient"
+});
+db.producto.insertOne({
+    _id: 19,
+    marca: "Amet Consectetuer Industries",
+    nombre: "receipt",
+    descripcion: "mollis. Phasellus libero"
+});
+db.producto.insertOne({
+    _id: 20,
+    marca: "Auctor Corporation",
+    nombre: "half price",
+    descripcion: "dictum placerat, augue."
+});
+db.producto.insertOne({
+    _id: 21,
+    marca: "Arcu Vestibulum Ut Consulting",
+    nombre: "household goods",
+    descripcion: "Quisque purus sapien,"
+});
+db.producto.insertOne({
+    _id: 22,
+    marca: "Eu Accumsan Sed Consulting",
+    nombre: "barcode",
+    descripcion: "magnis dis parturient"
+});
+db.producto.insertOne({
+    _id: 23,
+    marca: "Quam Industries",
+    nombre: "groceries",
+    descripcion: "tellus eu augue"
+});
+db.producto.insertOne({
+    _id: 24,
+    marca: "Enim Associates",
+    nombre: "",
+    descripcion: "eu elit. Nulla"
+});
+db.producto.insertOne({
+    _id: 25,
+    marca: "Dis Parturient Montes Consulting",
+    nombre: "bag",
+    descripcion: "Curabitur dictum. Phasellus"
+});
+db.producto.insertOne({
+    _id: 26,
+    marca: "Lectus Corporation",
+    nombre: " drugstore ",
+    descripcion: "risus, at fringilla"
+});
+db.producto.insertOne({
+    _id: 27,
+    marca: "Nec Tempus Foundation",
+    nombre: "credit card",
+    descripcion: "euismod mauris eu"
+});
+db.producto.insertOne({
+    _id: 28,
+    marca: "Vehicula Pellentesque LLP",
+    nombre: " drugstore ",
+    descripcion: "Morbi sit amet"
+});
+db.producto.insertOne({
+    _id: 29,
+    marca: "Convallis Ligula Donec Foundation",
+    nombre: "supervisor",
+    descripcion: "egestas lacinia. Sed"
+});
+db.producto.insertOne({
+    _id: 30,
+    marca: "Eu Lacus Consulting",
+    nombre: "fish",
+    descripcion: "vel nisl. Quisque"
+});
+db.producto.insertOne({
+    _id: 31,
+    marca: "Tempor Bibendum PC",
+    nombre: "refundable",
+    descripcion: "sit amet ornare"
+});
+db.producto.insertOne({
+    _id: 32,
+    marca: "Montes Nascetur Incorporated",
+    nombre: "cosmetics",
+    descripcion: "purus. Nullam scelerisque"
+});
+db.producto.insertOne({
+    _id: 33,
+    marca: "Turpis Nulla Corp.",
+    nombre: " drugstore ",
+    descripcion: "pede, nonummy ut,"
+});
+db.producto.insertOne({
+    _id: 34,
+    marca: "Eu Eleifend Nec Associates",
+    nombre: "canned goods",
+    descripcion: "orci sem eget"
+});
+db.producto.insertOne({
+    _id: 35,
+    marca: "Euismod Incorporated",
+    nombre: "chemist's ",
+    descripcion: "enim. Nunc ut"
+});
+db.producto.insertOne({
+    _id: 36,
+    marca: "Congue A Aliquet Corp.",
+    nombre: "sales assistant",
+    descripcion: "diam nunc, ullamcorper"
+});
+db.producto.insertOne({
+    _id: 37,
+    marca: "Sed Dui Fusce LLC",
+    nombre: "coin",
+    descripcion: "eleifend nec, malesuada"
+});
+db.producto.insertOne({
+    _id: 38,
+    marca: "Arcu Incorporated",
+    nombre: "bag",
+    descripcion: "justo eu arcu."
+});
+db.producto.insertOne({
+    _id: 39,
+    marca: "Donec Ltd",
+    nombre: "fresh flowers",
+    descripcion: "vitae odio sagittis"
+});
+db.producto.insertOne({
+    _id: 40,
+    marca: "Dolor Dolor Tempus Ltd",
+    nombre: "check-out counter",
+    descripcion: "Nullam lobortis quam"
+});
+db.producto.insertOne({
+    _id: 41,
+    marca: "Massa Integer Vitae Company",
+    nombre: "change",
+    descripcion: "mollis non, cursus"
+});
+db.producto.insertOne({
+    _id: 42,
+    marca: "Proin LLP",
+    nombre: "on sale",
+    descripcion: "nibh dolor, nonummy"
+});
+db.producto.insertOne({
+    _id: 43,
+    marca: "Amet Institute",
+    nombre: "shopping cart",
+    descripcion: "id magna et"
+});
+db.producto.insertOne({
+    _id: 44,
+    marca: "Nec Inc.",
+    nombre: "on sale",
+    descripcion: "penatibus et magnis"
+});
+db.producto.insertOne({
+    _id: 45,
+    marca: "Ultricies Foundation",
+    nombre: "check-out counter",
+    descripcion: "Praesent luctus. Curabitur"
+});
+db.producto.insertOne({
+    _id: 46,
+    marca: "Sed Dictum LLC",
+    nombre: "fridge",
+    descripcion: "Nunc ac sem"
+});
+db.producto.insertOne({
+    _id: 47,
+    marca: "Scelerisque Institute",
+    nombre: "shopping cart",
+    descripcion: "sapien. Cras dolor"
+});
+db.producto.insertOne({
+    _id: 48,
+    marca: "Arcu Morbi PC",
+    nombre: "aisle",
+    descripcion: "dignissim tempor arcu."
+});
+db.producto.insertOne({
+    _id: 49,
+    marca: "Velit Pellentesque Industries",
+    nombre: "ticket",
+    descripcion: "Suspendisse sagittis. Nullam"
+});
+db.producto.insertOne({
+    _id: 50,
+    marca: "Pede Malesuada Associates",
+    nombre: "credit card",
+    descripcion: "pede blandit congue."
+});
+db.producto.insertOne({
+    _id: 51,
+    marca: "Mi Pede Nonummy LLC",
+    nombre: "household goods",
+    descripcion: "lectus ante dictum"
+});
+db.producto.insertOne({
+    _id: 52,
+    marca: "Ornare Lectus Justo PC",
+    nombre: "freezer",
+    descripcion: "a tortor. Nunc"
+});
+db.producto.insertOne({
+    _id: 53,
+    marca: "Justo Nec Foundation",
+    nombre: "groceries",
+    descripcion: "rhoncus. Nullam velit"
+});
+db.producto.insertOne({
+    _id: 54,
+    marca: "Ipsum Curabitur LLP",
+    nombre: "banknote",
+    descripcion: "fringilla. Donec feugiat"
+});
+db.producto.insertOne({
+    _id: 55,
+    marca: "A Purus Duis Limited",
+    nombre: "receipt",
+    descripcion: "pellentesque a, facilisis"
+});
+db.producto.insertOne({
+    _id: 56,
+    marca: "Ad Litora Ltd",
+    nombre: "20% off",
+    descripcion: "Vivamus non lorem"
+});
+db.producto.insertOne({
+    _id: 57,
+    marca: "Mauris Blandit Mattis Ltd",
+    nombre: "sales assistant",
+    descripcion: "mauris id sapien."
+});
+db.producto.insertOne({
+    _id: 58,
+    marca: "In Consectetuer Ipsum Company",
+    nombre: "product",
+    descripcion: "fringilla. Donec feugiat"
+});
+db.producto.insertOne({
+    _id: 59,
+    marca: "Ultricies LLP",
+    nombre: "manager",
+    descripcion: "Nunc mauris. Morbi"
+});
+db.producto.insertOne({
+    _id: 60,
+    marca: "Egestas PC",
+    nombre: "packaging",
+    descripcion: "rhoncus. Nullam velit"
+});
+db.producto.insertOne({
+    _id: 61,
+    marca: "Dui Corporation",
+    nombre: "bakery",
+    descripcion: "et malesuada fames"
+});
+db.producto.insertOne({
+    _id: 62,
+    marca: "Nulla Dignissim Institute",
+    nombre: "grand opening sale",
+    descripcion: "eu tempor erat"
+});
+db.producto.insertOne({
+    _id: 63,
+    marca: "Mauris Sapien Corp.",
+    nombre: "fish",
+    descripcion: "rutrum urna, nec"
+});
+db.producto.insertOne({
+    _id: 64,
+    marca: "Ligula Eu LLC",
+    nombre: "lift",
+    descripcion: "nec orci. Donec"
+});
+db.producto.insertOne({
+    _id: 65,
+    marca: "Arcu Vestibulum Ante Limited",
+    nombre: "meat",
+    descripcion: "elit. Nulla facilisi."
+});
+db.producto.insertOne({
+    _id: 66,
+    marca: "Egestas Incorporated",
+    nombre: "price",
+    descripcion: "est ac facilisis"
+});
+db.producto.insertOne({
+    _id: 67,
+    marca: "Nibh Dolor Company",
+    nombre: "fruit",
+    descripcion: "amet, faucibus ut,"
+});
+db.producto.insertOne({
+    _id: 68,
+    marca: "Lacus Nulla Tincidunt Inc.",
+    nombre: "refundable",
+    descripcion: "ut quam vel"
+});
+db.producto.insertOne({
+    _id: 69,
+    marca: "Quam Dignissim Limited",
+    nombre: "fresh flowers",
+    descripcion: "sapien, cursus in,"
+});
+db.producto.insertOne({
+    _id: 70,
+    marca: "Quis Massa Foundation",
+    nombre: "lift",
+    descripcion: "lorem, auctor quis,"
+});
+db.producto.insertOne({
+    _id: 71,
+    marca: "Ipsum Donec Sollicitudin Corporation",
+    nombre: "customer",
+    descripcion: "Phasellus libero mauris,"
+});
+db.producto.insertOne({
+    _id: 72,
+    marca: "Metus Company",
+    nombre: "fruit",
+    descripcion: "sed pede. Cum"
+});
+db.producto.insertOne({
+    _id: 73,
+    marca: "Lobortis Tellus LLP",
+    nombre: "guarantee",
+    descripcion: "ut nisi a"
+});
+db.producto.insertOne({
+    _id: 74,
+    marca: "Tincidunt Industries",
+    nombre: "manager",
+    descripcion: "in, tempus eu,"
+});
+db.producto.insertOne({
+    _id: 75,
+    marca: "Aliquam Auctor Associates",
+    nombre: "groceries",
+    descripcion: "a, arcu. Sed"
+});
+db.producto.insertOne({
+    _id: 76,
+    marca: "Morbi Neque Tellus Incorporated",
+    nombre: "ticket",
+    descripcion: "non magna. Nam"
+});
+db.producto.insertOne({
+    _id: 77,
+    marca: "Netus Et Malesuada PC",
+    nombre: "check-out counter",
+    descripcion: "ac facilisis facilisis,"
+});
+db.producto.insertOne({
+    _id: 78,
+    marca: "Augue Eu Ltd",
+    nombre: "beverages",
+    descripcion: "Quisque purus sapien,"
+});
+db.producto.insertOne({
+    _id: 79,
+    marca: "Nec Cursus Inc.",
+    nombre: "till",
+    descripcion: "et magnis dis"
+});
+db.producto.insertOne({
+    _id: 80,
+    marca: "Suspendisse Tristique Company",
+    nombre: "guarantee",
+    descripcion: "nec urna et"
+});
+db.producto.insertOne({
+    _id: 81,
+    marca: "Vehicula Pellentesque Company",
+    nombre: "20% off",
+    descripcion: "risus. Donec nibh"
+});
+db.producto.insertOne({
+    _id: 82,
+    marca: "Morbi Consulting",
+    nombre: "nutritional information",
+    descripcion: "nascetur ridiculus mus."
+});
+db.producto.insertOne({
+    _id: 83,
+    marca: "Natoque Penatibus LLC",
+    nombre: "sales tax",
+    descripcion: "eget odio. Aliquam"
+});
+db.producto.insertOne({
+    _id: 84,
+    marca: "Suspendisse Eleifend Cras Limited",
+    nombre: "cheque",
+    descripcion: "blandit congue. In"
+});
+db.producto.insertOne({
+    _id: 85,
+    marca: "In Faucibus Inc.",
+    nombre: "return policy",
+    descripcion: "ultrices, mauris ipsum"
+});
+db.producto.insertOne({
+    _id: 86,
+    marca: "Cras Ltd",
+    nombre: "fresh flowers",
+    descripcion: "ultrices, mauris ipsum"
+});
+db.producto.insertOne({
+    _id: 87,
+    marca: "Enim Nisl Institute",
+    nombre: "barcode reader",
+    descripcion: "turpis egestas. Fusce"
+});
+db.producto.insertOne({
+    _id: 88,
+    marca: "Ornare Tortor Inc.",
+    nombre: "change",
+    descripcion: "auctor vitae, aliquet"
+});
+db.producto.insertOne({
+    _id: 89,
+    marca: "Curabitur Ut Corporation",
+    nombre: "fruit",
+    descripcion: "risus. Donec nibh"
+});
+db.producto.insertOne({
+    _id: 90,
+    marca: "Purus Ac Tellus LLC",
+    nombre: "delicatessen",
+    descripcion: "magna a tortor."
+});
+db.producto.insertOne({
+    _id: 91,
+    marca: "Neque Incorporated",
+    nombre: "refundable",
+    descripcion: "iaculis nec, eleifend"
+});
+db.producto.insertOne({
+    _id: 92,
+    marca: "At Auctor PC",
+    nombre: "trolley",
+    descripcion: "ipsum ac mi"
+});
+db.producto.insertOne({
+    _id: 93,
+    marca: "Egestas Ligula Inc.",
+    nombre: "fruit",
+    descripcion: "purus. Maecenas libero"
+});
+db.producto.insertOne({
+    _id: 94,
+    marca: "Auctor Quis LLP",
+    nombre: "shopping basket",
+    descripcion: "nibh. Quisque nonummy"
+});
+db.producto.insertOne({
+    _id: 95,
+    marca: "Accumsan Interdum Libero Inc.",
+    nombre: "household goods",
+    descripcion: "ut, pellentesque eget,"
+});
+db.producto.insertOne({
+    _id: 96,
+    marca: "Sed Hendrerit A Corporation",
+    nombre: "manager",
+    descripcion: "lacus. Aliquam rutrum"
+});
+db.producto.insertOne({
+    _id: 97,
+    marca: "Dictum Cursus Inc.",
+    nombre: "beverages",
+    descripcion: "orci. Ut semper"
+});
+db.producto.insertOne({
+    _id: 98,
+    marca: "In Ornare Sagittis Corporation",
+    nombre: "barcode",
+    descripcion: "hendrerit id, ante."
+});
+db.producto.insertOne({
+    _id: 99,
+    marca: "Fringilla Porttitor Vulputate Corporation",
+    nombre: "cash only",
+    descripcion: "consectetuer adipiscing elit."
+});
+db.producto.insertOne({
+    _id: 100,
+    marca: "Porttitor Interdum PC",
+    nombre: " drugstore ",
+    descripcion: "molestie in, tempus"
+});
