5403d0c42479f1258834639261bf252dd77ef0be
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-11-18 23:06:11 +0000

    Fixed container build and port fowarding

--- .devcontainer/populate.sh
+++ .devcontainer/populate.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-MARKER_FILE=".initialized"
-
-if [ -f "$MARKER_FILE" ]; then
-  echo "Already initialized"
-  exit 0
-fi
+echo "Starting populating DBs..."
 
 mongosh "mongodb://mongo:27017" -f ./mongo/schema/*.mongodb.js
 redis-cli -h redis -p 6379 < ./redis/schema/productos.redis
 
-echo "Delete me if you want to rerun the schema files on the devcontainer" > "$MARKER_FILE"
+echo "Done!"
